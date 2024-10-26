fe5dbd20f52c125310ef7f333062abd0b1a80df7
Áron ECSEDY <aron@milonga.hu>
2024-10-26 22:38:44 +0200

    CH 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -7,11 +8,28 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    drawable = updatable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+        
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    player_ship = Player(x,y)
+
+    #game loop
     while 1 == 1:
+        dt = clock.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        for each in drawable:
+            each.draw(screen)
+        for each in updatable:
+            each.update(dt)
+
         pygame.display.flip()
 
     
